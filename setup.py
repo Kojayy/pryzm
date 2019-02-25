17c853c1e087a189bfe24d3ea27db8db3bb14110
Niarfe <efrain.olivares@gmail.com>
2019-02-25 01:34:16 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='pryzm',
-    version='0.4',
-    description='Lightweight text coloring for clis',
-    url='https://Niarfe@github.org/Niarfe/pryzm.git',
-    author='Efrain Olivares',
-    author_email='efrain.olivares@gmail.com',
-    license='MIT',
-    packages=['pryzm'],
-    zip_safe=False
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pryzm",
+    version="0.0.4",
+    author="Efrain Olivares",
+    author_email="efrain.olivares@gmail.com",
+    description="Basic support for asci color text on linux terminals",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hydraseq/pryzm",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
-
