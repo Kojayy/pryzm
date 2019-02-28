600ae632a147881430061b47a35c4013865966ef
Niarfe <efrain.olivares@gmail.com>
2019-02-27 23:00:18 -0800

    release 0.0.5

--- pryzm/pryzm.py
+++ pryzm/pryzm.py
@@ -1,23 +1,18 @@
 
-import sys
-import time
-
-from subprocess import Popen, PIPE
-from pryzm.codes import cap27, text_attributes
+from pryzm.codes import text_attributes
 
 class Pryzm(object):
-    """
+    """pz = pryzm.Pryzm(); warn = pz._italic().yellow().red; warn("Achtung!")
         ASCII FORMAT:
-            ESC[AT;FG;AT;BGm<text>ESC[0m
+            ESC[AT;FG;BGm<text>ESC[0m
     """
-    _cap27 = cap27
     _text_attributes = text_attributes
     def __init__(self, text=None):
         self.text = text if text else ''
-        self.subject = text
         self.at = 0
         self.fg = 30
         self.bg = 40
+        self.ASC = u"\u001b["
         self.CLR = u"\u001b[0m"
 
         for feature, ansi_code in self._text_attributes.items():
@@ -34,19 +29,22 @@ class Pryzm(object):
 
             return: function, adds a function named 'color' wrapping test with ascii code.
         """
+        #print("create {} using {}".format(feature, ansi_code))
         def fn(self, text=None):
 
-            if feature.isupper():
+            if feature.startswith('_'):
+                self.at = ansi_code
+            elif feature.isupper():
                 self.bg = ansi_code
             elif feature.islower():
                 self.fg = ansi_code
-            elif feature.startswith('_'):
-                self.at = ansi_code
 
             if text:
                 self.text = text
+                saved_return = self.show()
+                print(self.show())
                 self.reset()
-                return self.show()
+                return saved_return
             else:
                 return self
 
@@ -56,10 +54,7 @@ class Pryzm(object):
         fn.__doc__ = "Apply {0} to text".format(feature)
 
     def show(self):
-        return u"\u001b[{};{};{}m{}{}".format(self.at, self.fg, self.bg, self.text, self.CLR)
-
-    def print(self):
-        print(self.show())
+        return u"{}{};{};{}m{}{}".format(self.ASC, self.at, self.fg, self.bg, self.text, self.CLR)
 
     def __str__(self):
         return self.show()
