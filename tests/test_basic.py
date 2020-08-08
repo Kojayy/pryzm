6f514053488adaefc603b19276f5c53182581c7e
EfrainOlivaresCD <efrain.olivares@carpe.io>
2020-08-07 22:35:55 -0700

    fix up the sys path import for the tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.append(os.path.normpath(os.path.join(os.path.dirname(__file__), "../")))
+absp = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, absp)
 
 import pryzm
 
