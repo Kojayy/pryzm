025af673de40a12bf4e65cd046a48717a541155b
EfrainOlivaresCD <efrain.olivares@carpe.io>
2020-08-08 00:55:47 -0700

    saving patch to multiple arguments

--- pryzm/pryzm.py
+++ pryzm/pryzm.py
@@ -7,14 +7,12 @@ class Pryzm(object):
             ESC[AT;FG;BGm<text>ESC[0m
     """
     _text_attributes = text_attributes
-    def __init__(self, text=None):
-        self.text = text if text else ''
-        self.at = 0
-        self.fg = 30
-        self.bg = 40
+    def __init__(self, *text, echo=False):
+        self.text = " ".join(text) 
         self.features = []
         self.ASC = u"\u001b["
         self.CLR = u"\u001b[0m"
+        self.echo = echo
 
         for feature, ansi_code in self._text_attributes.items():
             self._add_color(feature, ansi_code)
@@ -23,32 +21,32 @@ class Pryzm(object):
         self.features = []
         return self
 
-    def _add_color(self, feature, ansi_code, show=True):
+    def _add_color(self, feature, ansi_code):
         """Add dynamic function to insert color code.
             feature: string, the name of the function to add
             ansi_code: integer, the code value to insert
 
             return: function, adds a function named 'color' wrapping test with ascii code.
         """
-        def fn(self, text=None):
+        def fn(self, *text):
+            text = " ".join(text)
             self.features.append(str(ansi_code))
 
             if text:
                 self.text = text
                 saved_return = self.show()
-                if show: print(self.show())
+                if self.echo: print(self.show())
                 self.reset()
-                if show:
+                if self.echo:
                     return saved_return
                 else:
                     return
             else:
                 return self
 
-        setattr(Pryzm, feature, fn)
-
         fn.__name__ = feature
         fn.__doc__ = "Apply {0} to text".format(feature)
+        setattr(Pryzm, feature, fn)
 
     def show(self):
         return u"{}{}m{}{}".format(self.ASC, ";".join(self.features), self.text, self.CLR)
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,23 +6,34 @@ sys.path.insert(0, absp)
 import pryzm
 
 def test_basic_foreground():
-    pz = pryzm.Pryzm()
+    pz = pryzm.Pryzm(echo=True)
 
     assert pz.red("test")    == '\x1b[31mtest\x1b[0m'
     assert pz.cyan("test")   == '\x1b[36mtest\x1b[0m'
 
 def test_basic_background():
-    pz = pryzm.Pryzm()
+    pz = pryzm.Pryzm(echo=True)
 
     assert pz.RED("test")   == '\x1b[41mtest\x1b[0m'
 
 def test_basic_fore_back_combination():
-    pz = pryzm.Pryzm()
+    pz = pryzm.Pryzm(echo=True)
 
     assert pz.red().RED("test") == '\x1b[31;41mtest\x1b[0m'
 
 def test_create_function():
-    pz = pryzm.Pryzm()
+    pz = pryzm.Pryzm(echo=True)
 
     both_red = pz.red().RED
     assert both_red("test") == '\x1b[31;41mtest\x1b[0m'
+
+def test_multiple_arguments():
+    pz = pryzm.Pryzm(echo=True)
+
+    blue = pz.blue
+    assert blue("This is", "also blue") == '\x1b[34mThis is also blue\x1b[0m'
+
+def test_multiple_arguments_in_class():
+    pz = pryzm.Pryzm(echo=True)
+
+    assert pz.blue("This is", "also blue") == '\x1b[34mThis is also blue\x1b[0m'
